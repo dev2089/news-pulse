c7219c1633e451d9f5f5ad0912fc092035f414d1
dev2089 <devanshu2089@gmail.com>
2026-09-22 13:21:48 +0530

    Harden News Pulse api/index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,5 @@
 import { handle } from "../backend/index.js";
-export default async function handler(req,res){return handle(req,res)}
+
+export default async function handler(req, res) {
+  return handle(req, res);
+}
